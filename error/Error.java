be389b20aaf3af579c3920eb063597a370236ac1
ibongiovanni <ignaciobongiovanni@gmail.com>
2016-09-19 10:56:59 -0300

    Update Error class in error package

--- error/Error.java
+++ error/Error.java
@@ -1,5 +1,20 @@
 package error;
 
 public class Error {
-	public Error(int line, int col, String msg){}
+
+	private int line;
+	private int col;
+	private String msg;
+
+	public Error(int line, int col, String msg){
+		this.line=line;
+		this.col=col;
+		this.msg=msg;
+	}
+
+	@Override
+	public String toString(){
+		return "ERROR, in line: "+line+", column: "+col+". '"+msg+"'";	
+	}
+
 }
